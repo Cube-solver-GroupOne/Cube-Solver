55469acfc424df63cd2c1c7617dbac9e1dc90272
mohammad-alshish <eng.moahammed.majed@gamil.com>
2022-07-20 21:43:24 +0300

    some tests

--- cube_steps_3D/color_detect.py
+++ cube_steps_3D/color_detect.py
@@ -165,7 +165,7 @@ class CaptureCube:
             return 'n'
 
     def capture_cube(self):
-        #  to turn on the camera and  end it
+        #  to turn on the camera and end it
         cap = cv2.VideoCapture(0)
         cv2.namedWindow('CaptureCube')
         font = cv2.FONT_HERSHEY_SIMPLEX
